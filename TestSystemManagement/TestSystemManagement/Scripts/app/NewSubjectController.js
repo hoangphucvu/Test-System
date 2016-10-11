0481d5a8bf733c1449a52b91d4db4a2ed301387e
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-10-11 20:48:08 +0700

    Loading effect

--- TestSystemManagement/TestSystemManagement/Scripts/app/NewSubjectController.js
+++ TestSystemManagement/TestSystemManagement/Scripts/app/NewSubjectController.js
@@ -11,7 +11,8 @@
     function NewSubjectController($scope, $http, NewSubjectService) {
         $scope.Submitted = false;
         $scope.IsFormValid = false;
-
+        $scope.Loading = false;
+        $scope.HideAddSubjectBtn = false;
         $scope.SubjectData = {
             SubjectName: '',
             SubjectCode: '',
@@ -28,13 +29,18 @@
         $scope.AddNewSubject = function () {
             $scope.Submitted = true;
             if ($scope.IsFormValid) {
+                $scope.Loading = true;
+                $scope.HideAddSubjectBtn = true;
                 NewSubjectService.NewSubject($scope.SubjectData).then(function (result) {
-                    console.log(result);
                     if (result.data === 'success') {
                         alert('Thêm môn thành công');
+                        $scope.Loading = false;
+                        $scope.HideAddSubjectBtn = false;
                     }
                     else {
                         alert('Vui lòng kiểm tra dữ liệu');
+                        $scope.Loading = false;
+                        $scope.HideAddSubjectBtn = false;
                     }
                 });
             }
