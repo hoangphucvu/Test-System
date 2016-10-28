135d51660615ec105879ee3c4faba8ae43f53397
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-10-28 20:25:56 +0700

    Update missing helper

---
+++ TestSystemManagement/TestSystemManagement.Repository/Config/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace TestSystemManagement.Repository.Config
+{
+    public class Helper
+    {
+        public static readonly string ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+        public static readonly string TableDetails = "TestDetails";
+    }
+}
